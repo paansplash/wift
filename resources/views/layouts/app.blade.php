8861c3fe04303d97a9063eef75e2b5252109a14c
Farhan Afandi <farhanafandi.ar@gmail.com>
2025-01-06 08:10:50 +0800

    initial

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Basic -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('title', 'Default Title')</title>
+
+    <meta name="keywords" content="@yield('meta_keywords', 'WebSite Template')" />
+    <meta name="description" content="@yield('meta_description', 'Porto - Multipurpose Website Template')">
+    <meta name="author" content="@yield('meta_author', 'okler.net')">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ asset('assets/img/favicon.ico') }}" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="{{ asset('assets/img/apple-touch-icon.png') }}">
+
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
+
+    <!-- Web Fonts -->
+    <link id="googleFonts" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800%7COverpass:200,400,600,700,800,900%7CPT+Serif&display=swap"
+        rel="stylesheet" type="text/css">
+
+    <!-- Vendor CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/fontawesome-free/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/animate/animate.compat.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/simple-line-icons/css/simple-line-icons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/owl.carousel/assets/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/owl.carousel/assets/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/magnific-popup/magnific-popup.min.css') }}">
+
+    <!-- Theme CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/theme.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/theme-elements.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/theme-blog.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/theme-shop.css') }}">
+
+    <!-- Demo CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/demos/demo-architecture-2.css') }}">
+
+    <!-- Skin CSS -->
+    <link id="skinCSS" rel="stylesheet" href="{{ asset('assets/css/skins/skin-architecture-2.css') }}">
+
+    <!-- Theme Custom CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
+
+    @stack('head')
+</head>
+
+<body class="loading-overlay-showing" data-loading-overlay data-plugin-page-transition>
+    <div class="loading-overlay">
+        <div class="bounce-loader">
+            <div class="bounce1"></div>
+            <div class="bounce2"></div>
+            <div class="bounce3"></div>
+        </div>
+    </div>
+
+    <div class="body">
+        @yield('content')
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('assets/vendor/plugins/js/plugins.min.js') }}"></script>
+    <script src="{{ asset('assets/js/theme.js') }}"></script>
+    <script src="{{ asset('assets/js/demos/demo-architecture-2.js') }}"></script>
+    <script src="{{ asset('assets/js/custom.js') }}"></script>
+    <script src="{{ asset('assets/js/theme.init.js') }}"></script>
+    
+    @stack('scripts')
+</body>
+
+</html>
